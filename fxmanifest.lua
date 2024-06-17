109a97af69ee2f3041c17d307f3b279f8d57c536
Ralfnl12 <78805502+Ralfnl12@users.noreply.github.com>
2024-06-17 11:45:40 +0200

    update

---
+++ fxmanifest.lua
@@ -0,0 +1,21 @@
+fx_version 'cerulean'
+game 'gta5'
+
+description 'RC_Sollicitaties'
+version '0.0.1'
+author 'Ralfnl12'
+
+ui_page 'html/index.html'
+
+files {
+    'html/index.html',
+    'html/style.css',
+    'html/script.js',
+    'html/sollipaginas/ambu.html',
+    'html/sollipaginas/kmar.html',
+    'html/sollipaginas/politie.html',
+    'html/sollipaginas/anwb.html',
+}
+
+client_script 'client.lua'
+-- server_script 'server.lua'
